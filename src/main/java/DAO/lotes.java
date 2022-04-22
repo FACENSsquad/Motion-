4c92b2322923a1ac68e517dccbbc947c24354eb4
João melo <joaomelo_9@hotmail.com>
2022-04-22 10:18:47 -0300

    att

--- src/main/java/DAO/lotes.java
+++ src/main/java/DAO/lotes.java
@@ -4,6 +4,13 @@ LOTES
 
 package DAO;
 
+/*
+PRODUTO
+ALOCAÇÃO
+QUANTIDADE
+VALOR
+DATA DE VALIDADE
+*/
 public class lotes {
     
 }
--- src/main/java/DAO/produtos.java
+++ src/main/java/DAO/produtos.java
@@ -1,14 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+PRODDUTOS
  */
 package DAO;
 
-/**
- *
- * @author joaom
- */
+/*
+PRODUTO
+TIPO 
+NCM
+PONTO DE PEDIDO
+*/
+
 public class produtos {
     
 }
